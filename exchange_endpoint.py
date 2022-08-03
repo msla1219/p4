b7df77c2402e02236cad9b3b2462d13df1ae6535
msla1219 <76463854+msla1219@users.noreply.github.com>
2022-08-03 17:42:21 +0900

    Update exchange_endpoint.py

--- exchange_endpoint.py
+++ exchange_endpoint.py
@@ -64,8 +64,96 @@ def verify(content):
         print(e)
 
         
-def fill_order(order,txes=[]):
-    pass
+def process_order(content):
+
+  #1. Insert new order
+
+    order_obj = Order(sender_pk=content['payload']['sender_pk'],
+                      receiver_pk=content['payload']['receiver_pk'], 
+                      buy_currency=content['payload']['buy_currency'], 
+                      sell_currency=content['payload']['sell_currency'], 
+                      buy_amount=content['payload']['buy_amount'], 
+                      sell_amount=content['payload']['sell_amount'], 
+                      exchange_rate=(content['payload']['buy_amount']/content['payload']['sell_amount']),
+                      signature=content['sig'])
+
+
+
+    g.session.add(order_obj)
+    g.session.commit()
+
+    # check up if it works well and get the order id
+    results = g.session.execute("select distinct id from orders where " + 
+                            " sender_pk = '" + str(order['sender_pk']) + "'" +
+                            " and receiver_pk = '" + str(order['receiver_pk']) + "'")
+
+    order_id = results.first()['id']
+    # print(" new order: ", order_id, order['buy_currency'], order['sell_currency'], order['buy_amount'], order['sell_amount'])
+
+    #2. Matching order
+    results = g.session.execute("select count(id) " + 
+                            " from orders where orders.filled is null " + 
+                            " and orders.sell_currency = '" + order['buy_currency'] + "'" +
+                            " and orders.buy_currency = '" + order['sell_currency'] + "'" +
+                            " and exchange_rate <= " + str(order['sell_amount']/order['buy_amount']))
+
+    if results.first()[0] == 0:
+        # print("::::no matching order::::")
+        return
+
+    results = g.session.execute("select distinct id, sender_pk, receiver_pk, buy_currency, sell_currency, buy_amount, sell_amount " + 
+                            "from orders where orders.filled is null " + 
+                            " and orders.sell_currency = '" + order['buy_currency'] + "'" +
+                            " and orders.buy_currency = '" + order['sell_currency'] + "'" +
+                            " and exchange_rate <= " + str(order['sell_amount']/order['buy_amount'])) 
+
+    for row in results:
+        m_order_id = row['id']
+        m_sender_pk = row['sender_pk']
+        m_receiver_pk = row['receiver_pk'] 
+        m_buy_currency = row['buy_currency'] 
+        m_sell_currency = row['sell_currency'] 
+        m_buy_amount = row['buy_amount']
+        m_sell_amount = row['sell_amount']
+        # print(" matched at ID: ", m_order_id)
+        break
+
+    # print(" matching order: ", m_order_id, m_buy_currency, m_sell_currency, m_buy_amount, m_sell_amount)
+    # print(" order['sell_amount']/order['buy_amount']: ", order['sell_amount']/order['buy_amount'], ">=", "(buy_amount/sell_amount)", (m_buy_amount/m_sell_amount))
+
+    # update both the matching orders 
+    stmt = text("UPDATE orders SET counterparty_id=:id, filled=:curr_date WHERE id=:the_id")
+    stmt = stmt.bindparams(the_id=order_id, id=m_order_id, curr_date=datetime.now())
+    g.session.execute(stmt)  # where session has already been defined
+
+    stmt = text("UPDATE orders SET counterparty_id=:id, filled=:curr_date WHERE id=:the_id")
+    stmt = stmt.bindparams(the_id=m_order_id, id=order_id, curr_date=datetime.now())
+    g.session.execute(stmt)  # where session has already been defined
+  
+    #3. Create derived order
+    if order['buy_amount'] > m_sell_amount:
+        order_obj = Order(  sender_pk=order['sender_pk'],
+                            receiver_pk=order['receiver_pk'], 
+                            buy_currency=order['buy_currency'], 
+                            sell_currency=order['sell_currency'], 
+                            buy_amount=order['buy_amount'] - m_sell_amount, 
+                            sell_amount=order['sell_amount'] - ((order['sell_amount']/order['buy_amount']) * m_sell_amount),
+                            exchange_rate = (order['buy_amount'] - m_sell_amount)/(order['sell_amount'] - ((order['sell_amount']/order['buy_amount']) * m_sell_amount)),
+                            creator_id=order_id)
+        g.session.add(order_obj)
+        g.session.commit()
+
+    elif order['buy_amount'] < m_sell_amount:
+        order_obj = Order(  sender_pk=m_sender_pk,
+                            receiver_pk=m_receiver_pk, 
+                            buy_currency=m_buy_currency, 
+                            sell_currency=m_sell_currency, 
+                            buy_amount= m_buy_amount - (m_buy_amount/m_sell_amount) * order['buy_amount'], 
+                            sell_amount= m_sell_amount - order['buy_amount'],
+                            exchange_rate = (m_buy_amount - (m_buy_amount/m_sell_amount) * order['buy_amount'])/(m_sell_amount - order['buy_amount']),
+                            creator_id=m_order_id)
+        g.session.add(order_obj)
+        g.session.commit()
 
 
 def log_message(d):
@@ -112,16 +200,14 @@ def trade():
                 return jsonify( False )
             
         #Your code here
-        #Note that you can access the database session using g.session
 
-        # TODO: Check the signature
-        
-        # TODO: Add the order to the database
-        
-        # TODO: Fill the order
-        
-        # TODO: Be sure to return jsonify(True) or jsonify(False) depending on if the method was successful
-        
+        #Note that you can access the database session using g.session
+        if verify(content) is True: 
+            process_order(content)
+        else:
+            log_message(content)
+
+        return jsonify( True )
 
 @app.route('/order_book')
 def order_book():
